97ce12d3cf8142ee9643bb6aeb03305fbb025395
Preston Chaplin <preston.chaplin@gmail.com>
2019-01-25 23:41:24 -0500

    friday night commit

--- squares.js
+++ squares.js
@@ -1,76 +1,52 @@
-const colors = ["blue", "green", "yellow", "orange"];
+const colors = ['#556270', '#4ECDC4', "#C7F464", "#FF6B6B", "#C44D58" ];
 
-// const inBox = document.querySelectorAll('.square')
+
 
 const generateDom = (arr) => {
 
-  for (let i = 0 ; i < arr.length; i++){
-    const curEl = arr[i];
-    let div = document.createElement('div');
-    div.id = color
-    div.className = '.square'
-    const newDiv = document.getElementById('div1');
-    document.body.insertBefore(div, newDiv)
-  }
-  
-  // const ul = document.querySelector('ul');
+  const squaresContainer = document.querySelector('#squaresContainer');
+  const buttonsContainer = document.querySelector('#buttonsContainer');
+  const hideAllButton = document.querySelector('#hide');
+  const showAllButton = document.querySelector('#show');
 
-  // const generateButton = document.querySelector('#generate');
+  hideAllButton.addEventListener('click', function(){
+    document.querySelectorAll('.square').forEach( square => {
+      square.style.display = 'none';
+    });
+  });
 
-  // generateButton.addEventListener('click', function(){
-  
-    // arr.forEach( color => {
-    //   let div = document.createElement('div');
-    //   div.id = color
-    //   div.className = '.square'
-    //   const newDiv = document.getElementById('div1');
-    //   document.body.insertBefore(div, newDiv)
-      // color.style.display = '';
+  showAllButton.addEventListener('click', function(){
+    document.querySelectorAll('.square').forEach( square => {
+      square.style.display = '';
+    });
+  });
 
-      
+  arr.forEach( color => {
+
+    const square = document.createElement('div');
+    square.className = 'square';
+    // square.innerHTML = color;
+    square.style.backgroundColor = color;
+    squaresContainer.appendChild(square);
+
+    const button = document.createElement('button');
+    button.innerHTML = `toggle`;
+    button.style.backgroundColor = color;
+    buttonsContainer.append(button);
+    button.addEventListener('click', function(){
+      if (square.style.display === 'none'){
+        square.style.display = '';
+      } else {
+        square.style.display = 'none';
+      }
+    });
   })
-// });
-
-  const hideButton = document.querySelector('#hide');
-
-  hideButton.addEventListener('click', function(){
-    const squares = document.querySelectorAll('.square');
-    squares.forEach( square => square.style.display = 'none');
-  });
-
-  
-
-  // const generateButton = document.querySelector('#generate');
-
-  // generateButton.addEventListener('click', function(){
-  //   colors.forEach( color => {
-  //     const createdSquares = document.createElement('div');
-  //     createdSquares.className = '.square';
-  //     createdSquares.style.display = '';
-  //     box.appendChild(createdSquares)
-  //     // createdSquares.id = color
-  //     })
-  //   });
-
-  const showButton = document.querySelector('#show');
-
-  showButton.addEventListener('click', function(){
-  const squares = document.querySelectorAll('.square');
-  squares.forEach( square => square.style.display = '');
-  });
-
-  const toggleRed = document.querySelector('#toggleRed');
-
-  const redSquare = document.querySelector('#red');
-
-  toggleRed.addEventListener('click', function (){
-    if (redSquare.style.display === ''){
-      redSquare.style.display = 'none';
-    } else if (redSquare.style.display === 'none'){
-      redSquare.style.display = '';
-    }
-  });
 
 };
 
-generateDom(colors); //should generate squares, buttons and event handlers
+generateDom(colors);
+generateDom(colors);
+generateDom(colors);
+generateDom(colors);
+generateDom(colors);
+
